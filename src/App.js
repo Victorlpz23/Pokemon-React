8bab680c0df4839e84e0ae8ec2dbf5976196b7e4
Victor Lopez <victorlpz2301@gmail.com>
2023-03-27 11:18:20 +0200

    Pokemon Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Pokemon from './components/Pokemon/Pokemon';
+import { getPokemons } from './services/PokemonService'
 
 function App() {
+  
   return (
     <div className="App">
+    <Pokemon pokemon="Bulbasur"/>
     </div>
   );
 }
---
+++ src/components/Pokemon/Pokemon.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Pokemon({ pokemon }) {
+  return (
+    <div>Pokemon {pokemon}</div>
+  )
+}
+
+export default Pokemon
