89f36a8ca5db84d941a4a8b833cb2f597f69a41e
Victor Lopez <victorlpz2301@gmail.com>
2023-03-27 11:35:09 +0200

    Pokemon Component Done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Pokemon from './components/Pokemon/Pokemon';
 import { getPokemons } from './services/PokemonService'
 
 function App() {
-  
+  const [pokemonsData, setPokemonsData] = useState([]);
+
+  useEffect(() => {
+    getPokemons()
+      .then((pokemons) => setPokemonsData(pokemons))
+  }, [])
+
+
   return (
     <div className="App">
-    <Pokemon pokemon="Bulbasur"/>
+      <Pokemon pokemon={pokemonsData} />
     </div>
   );
 }
--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
 function Pokemon({ pokemon }) {
+  console.log(pokemon[0].name.english)
   return (
-    <div>Pokemon {pokemon}</div>
+    <div>Pokemon {pokemon[0].name.english}</div>
   )
 }
 
